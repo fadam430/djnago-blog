d8b6c1b18c435c93c0333e546a8626609a80069a
fadam430 <f.adam430@gmail.com>
2025-11-24 14:19:59 +0000

    create a about app

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path # type: ignore
 
 urlpatterns = [
     path("", views.PostList.as_view(), name='home'),
-    path("slug:<slug>/", views.post_detail, name='post_detail'),
+    path("<slug:slug>/", views.post_detail, name='post_detail'),
+    #path("<int:event_id>/", views.event_detail, name='event_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404 # type: ignore
 from django.views import generic # type: ignore
 from .models import Post
+#from .models import Event
 
 # Create your views here.
 class PostList(generic.ListView):
@@ -8,7 +9,7 @@ class PostList(generic.ListView):
     template_name = 'blog/index.html'
     paginate_by = 6
     
-def post_detail(request,  slug):
+def post_detail(request, slug):
     """
     Renders the detail view of a blog post identified by its slug.
     
@@ -25,3 +26,30 @@ def post_detail(request,  slug):
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
     return render(request, 'blog/post_detail.html', {'post': post}) 
+
+# class EventList(generic.ListView):
+#     model = Event
+#     template_name = "index.html"
+#     paginate_by = 12
+
+
+
+# def event_detail(request, event_id):
+#     """
+#     Renders the detail view of an event identified by its ID.
+    
+#     **Content**
+    
+#     ``event_detail``
+#         an instance of model : `blog.event`
+        
+#     **Template**
+    
+#     :template:`blog/event_detail.html`
+#     """
+    
+#     # Assuming there's an Event model similar to Post
+
+#     queryset = Event.objects.all()
+#     event = get_object_or_404(queryset, id=event_id)
+#     return render(request, 'events/event_detail.html', {'event': event})
